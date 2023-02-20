93dd644d0b8a4dce166505dd90b37ad158399dc3
Antoine Revel <antoine.revel@gmail.com>
2023-02-20 01:56:11 +0100

    ex 1 and 2 on the same transaction

--- contracts/AntoineRevel_SolutionTest.sol
+++ contracts/AntoineRevel_SolutionTest.sol
@@ -2,6 +2,9 @@ pragma solidity ^0.6.2;
 import "./Evaluator.sol";
 import "./DummyToken.sol";
 
+import "./utils/IUniswapV2Factory.sol";
+import "./utils/IUniswapV2Pair.sol";
+
 import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -9,48 +12,55 @@ contract AntoineRevel_SolutionTest {
     Evaluator evaluator;
     ERC20 tdToken;
     IExerciceSolution solution;
-    bool isStarted;
+
 
     IUniswapV2Router02 uniswapRouter;
-    DummyToken dummyToken;
+    IUniswapV2Factory public uniswapV2Factory;
+
+    address dummyTokenAddress;
+    DummyToken private dummyToken;
+
+    address wethAddress;
     IERC20 private weth;
 
-
-    constructor(IExerciceSolution _solution, Evaluator _evaluator,address _uniswapRouterAddress,DummyToken _dummyToken,address _tdToken) public{
-        evaluator = _evaluator;
+    constructor(IExerciceSolution _solution, address payable _evaluator,address _uniswapRouterAddress,address _dummyToken,address _tdToken) public{
+        evaluator = Evaluator(_evaluator);
         tdToken=ERC20(_tdToken);
         solution = _solution;
-        isStarted=false;
         uniswapRouter=IUniswapV2Router02(_uniswapRouterAddress);
-        dummyToken=_dummyToken;
-        weth=IERC20(uniswapRouter.WETH());
+        uniswapV2Factory=IUniswapV2Factory(uniswapRouter.factory());
+        dummyTokenAddress=_dummyToken;
+        dummyToken=DummyToken(_dummyToken);
+        wethAddress=uniswapRouter.WETH();
+        weth=ERC20(wethAddress);
     }
 
-    function startClaimPoint() public {
-        require(!isStarted,"Already started !");
-        isStarted=true;
-
-        evaluator.ex1_showIHaveTokens();
-        //evaluator.submitExercice(solution);
+    function start() external payable {
+        buyTokenEx1();
+        addLiquidityEx2();
     }
 
-    function buyToken() public payable {
-
+    function buyTokenEx1() private {
         address [] memory path  = new address[](2);
-        path[0] = uniswapRouter.WETH();
-        path[1]= address(dummyToken);
-        uint256[] memory amounts = uniswapRouter.swapExactETHForTokens{ value: msg.value }(0, path, address(this), block.timestamp + 15);
-
+        path[0] = wethAddress;
+        path[1]= dummyTokenAddress;
+        uniswapRouter.swapExactETHForTokens{ value: msg.value/2 }(0, path, address(this), block.timestamp + 15);
+        evaluator.ex1_showIHaveTokens();
     }
+
+    function addLiquidityEx2() private {
+        uint256 balanceDT=dummyToken.balanceOf(address(this));
+        require(dummyToken.approve(address(uniswapRouter), balanceDT),"token approved failed");
+        uniswapRouter.addLiquidityETH{ value: msg.value/2 }(dummyTokenAddress,balanceDT,0,0,address(this),block.timestamp + 15);
+        evaluator.ex2_showIProvidedLiquidity();
+    }
+
 
 
     function getPoint() public view returns (uint256){
         return tdToken.balanceOf(address(this));
     }
 
-    function getFactory() public view returns (address){
-        return uniswapRouter.factory();
-    }
 
     receive() external payable {}
 }
--- migrations/2_solution_deployer.js
+++ migrations/2_solution_deployer.js
@@ -1,8 +1,8 @@
 var ExerciceSolution = artifacts.require("ExerciceSolution.sol");
 var ExerciceSolutionTest = artifacts.require("AntoineRevel_SolutionTest.sol");
 
-var EvaluatorFile = artifacts.require("Evaluator.sol");
-var DummyTokenFile = artifacts.require("DummyToken.sol");
+var Evaluator="0xbF1D55027644401a4d3865536E4d94a0E34F15e6";
+var DummyToken="0x2aF483edaE4cce53186E6ed418FE92f8169Ad74E";
 
 
 var UniswapV2Router02="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
@@ -11,7 +11,6 @@ var tdToken="0x22E065dAE8e21d31ca04c1695d464D28C7b6014B";
 module.exports = (deployer, network, accounts) => {
     deployer.then(async () => {
         if (network == "goerli") {
-            await setStaticContracts();
             await deployExerciceSolution();
         } else if (network == "ganache") {
             await deploylocalExerciceSolution();
@@ -20,15 +19,10 @@ module.exports = (deployer, network, accounts) => {
     });
 };
 
-async function setStaticContracts() {
-    Evaluator = await EvaluatorFile.at("0xbF1D55027644401a4d3865536E4d94a0E34F15e6");
-    DummyToken = await DummyTokenFile.at("0xbF1D55027644401a4d3865536E4d94a0E34F15e6");
-}
-
 
 async function deployExerciceSolution() {
     mySolution = await ExerciceSolution.new();
-    myTest = await ExerciceSolutionTest.new(mySolution.address, Evaluator.address,UniswapV2Router02,DummyToken.address,tdToken);
+    myTest = await ExerciceSolutionTest.new(mySolution.address, Evaluator,UniswapV2Router02,DummyToken,tdToken);
 }
 
 async function deploylocalExerciceSolution() {
